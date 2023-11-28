8f9821c62585ab68db6c72dddff670bc7ce238bc
Aditya Fauzian Rifkikah Pati <55432346+Aditya-Fauzian-Rifkikah-Patty@users.noreply.github.com>
2023-11-28 11:52:59 +0700

    UPDATE LOGIN.VUE
    UPDATE LOGIN.VUE

--- Frontend/vue-project/src/views/login.vue
+++ Frontend/vue-project/src/views/login.vue
@@ -1,3 +1,109 @@
-<template>
-    <h1>Beranda</h1>
-</template>
+<template class="template">
+    <div class="centered-form">
+      <div class="form-container">
+        <form class="form">
+          <p class="form-title">Login</p>
+          <div class="input-container">
+            <input type="username" placeholder="Enter Username" />
+            <span></span>
+          </div>
+          <div class="input-container">
+            <input type="password" placeholder="Enter password" />
+          </div>
+          <button type="submit" class="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .form {
+    background-color: #fff;
+    display: block;
+    padding: 1rem;
+    max-width: 350px;
+    border-radius: 0.5rem;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  }
+  
+  .template {
+    /* padding: 3%; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* Memastikan form berada di tengah vertikal di seluruh layar */
+  }
+  
+  .centered-form {
+    text-align: center;
+    margin-top: 6%;
+  }
+  
+  .form-container {
+    display: inline-block;
+    background-color: #fff;
+    /* padding: 1rem; */
+    max-width: 350px;
+    border-radius: 0.5rem;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  }
+  
+  .form-title {
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+    font-weight: 600;
+    text-align: center;
+    color: #000;
+  }
+  
+  .input-container {
+    position: relative;
+  }
+  
+  .input-container input,
+  .form button {
+    outline: none;
+    border: 1px solid #e5e7eb;
+    margin: 8px 0;
+  }
+  
+  .input-container input {
+    background-color: #fff;
+    padding: 1rem;
+    padding-right: 3rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    width: 300px;
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  }
+  
+  .submit {
+    display: block;
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    background-color: #4f46e5;
+    color: #ffffff;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    font-weight: 500;
+    width: 100%;
+    border-radius: 0.5rem;
+    text-transform: uppercase;
+  }
+  
+  .signup-link {
+    color: #6b7280;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    text-align: center;
+  }
+  
+  .signup-link a {
+    text-decoration: underline;
+  }
+  </style>
+  
