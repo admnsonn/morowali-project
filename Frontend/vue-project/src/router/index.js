2f45d1de395fccc8c697124c12305acd9e832da4
Aditya Fauzian Rifkikah Pati <55432346+Aditya-Fauzian-Rifkikah-Patty@users.noreply.github.com>
2023-11-29 17:45:01 +0700

    update
    update

--- Frontend/vue-project/src/router/index.js
+++ Frontend/vue-project/src/router/index.js
@@ -1,38 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-<<<<<<< Updated upstream
-import BerandaView from "../views/beranda.vue"
-import PemerintahView from "../views/pemerintah.vue"
-import ProfilView from "../views/profildesa.vue"
-import DatadesaView from "../views/datadesa.vue"
-import DestinasiView from "../views/destinasi.vue"
-import IdmView from "../views/idm.vue"
-import InformasiView from "../views/informasi.vue"
-import LoginView from "../views/login.vue"
-import MediaView from "../views/media.vue"
-import PotensiView from "../views/potensi.vue"
-import WilayahView from "../views/wilayah.vue"
-import BeritaView from "../views/berita.vue"
-import SejarahView from "../views/sejarah.vue"
-import ContentView from "../views/contentberita.vue"
-
-const routes = [
-    { path: '', component: BerandaView },
-    { path: '/profil-desa', component: ProfilView },
-    { path: '/pemerintah-desa', component: PemerintahView },
-    { path: '/data-desa', component: DatadesaView },
-    { path: '/destinasi', component: DestinasiView },
-    { path: '/idm', component: IdmView },
-    { path: '/informasi', component: InformasiView },
-    { path: '/login', component: LoginView },
-    { path: '/media', component: MediaView },
-    { path: '/potensi-desa', component: PotensiView },
-    { path: '/sejarah-desa', component: PotensiView },
-    { path: '/wilayah', component: WilayahView },
-    { path: '/berita', component: BeritaView },
-    { path: '/sejarah', component: SejarahView },
-    { path: '/contentberita', component: ContentView },
-]
-=======
 import BerandaView from "../views/beranda.vue";
 import PemerintahView from "../views/pemerintah.vue";
 import ProfilView from "../views/profildesa.vue";
@@ -81,7 +47,6 @@ const routes = [
   { path: '/sejarah', component: SejarahView },
   { path: '/beranda-admin', component: BerandaAdmin, meta: { requiresAuth: true }, beforeEnter: authGuardAdmin }
 ];
->>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(),
