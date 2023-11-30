cb2e5016133a364de36c76595f062d88fc046817
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2023-11-30 21:50:25 +0700

    pemerintah desa (CLEAR)

--- Frontend/vue-project/src/views/pemerintah.vue
+++ Frontend/vue-project/src/views/pemerintah.vue
@@ -1,13 +1,64 @@
 <template>
     <div class="p-5 bg-utama">
         <h1 class="text-title mb-4 mt-0 pb-10">Pemerintahan Desa Bahomoleo</h1>
-        <div class="container-lg mt-5 sejarah-desa">
+        <div class="container-lg mt-5">
             <div class="row">
                 <div class="col-md-6">
-                    <img :src="imageUrl" alt="Desa Lamberea" class="rounded mx-auto d-block custom-ukuran" />
+                    <img :src="imageUrl" alt="Desa Bahomoleo" class="rounded mx-auto d-block custom-ukuran" />
                 </div>
                 <div class="col-md-6">
-                    <p v-for="(paragraph, index) in paragraphs" :key="index" class="sejarah-paragraph">{{ paragraph }}</p>
+                    <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-title">{{ paragraph }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-lg mt-5">
+        <h4 class="text-visi mb-4">Visi</h4>
+        <p v-for="(visis, index) in visi_misi" :key="index" class="penjelasan-vismis">{{ visis }}</p>
+    </div>
+    <br>
+    <div>
+        <h4 class="text-visi mb-4">Misi</h4>
+        <p v-for="(visis, index) in visi_misi" :key="index" class="penjelasan-vismis">{{ visis }}</p>
+    </div>
+
+    <br>
+    <div class="mt-5 p-5 bg-kedua">
+        <h2 class="text-subtitle mb-4 mt-0 pb-10">Program Kerja</h2>
+        <div class="container-lg mt-5">
+            <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-subtitle">{{ paragraph }}</p>
+        </div>
+    </div>
+
+    <div class="mt-5 p-5">
+        <h2 class="text-subtitle mb-4 mt-0 pb-10">Peraturan Kerja</h2>
+        <div class="container-lg mt-5">
+            <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-subtitle">{{ paragraph }}</p>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <div class="border rounded row align-items-center pt-3 pb-3 mb-4 with-shadow">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="~@/assets/Artikel.png" alt="Gambar Berita" class="img-fluid">
+                </div>
+
+                <div class="col-md-6 order-md-8">
+                    <div class="text-center text-md-start">
+                        <br>
+                        <div class="d-none d-md-block">
+                            <h2 class="mb-2 judul-artikel">
+                                {{ title_artikel }}
+                            </h2>
+                        </div>
+                        <br>
+                        <h5 class="mb-4 subjudul-artikel">{{ subtitle_artikel }}</h5>
+                            <button class="btn btn-secondary" @click="showDetail(item)">
+                                Selengkapnya
+                            </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,6 +71,9 @@ export default {
         return {
             imageUrl: "",
             paragraphs: "",
+            visi_misi: "",
+            title_artikel: "",
+            subtitle_artikel: "",
         };
     },
     methods: {
@@ -31,10 +85,16 @@ export default {
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis urna, vehicula ut felis non, venenatis lacinia urna. Nullam eget nibh ante. Phasellus sed mi id turpis luctus congue ut sed neque. Pellentesque quis pretium justo, convallis imperdiet felis. Proin porttitor tristique varius.",
                         "Integer non convallis dui, ac cursus neque. Curabitur facilisis scelerisque lacus quis bibendum. Mauris placerat dolor sem, sit amet vestibulum magna venenatis eu. Aliquam consectetur metus sapien, in gravida enim elementum id. Nulla et purus magna. Vestibulum porta vitae ipsum nec facilisis. Phasellus efficitur luctus enim vitae condimentum. Curabitur commodo purus ac mi luctus, vitae lobortis ante semper. Duis quam orci, rhoncus vitae ullamcorper quis, tincidunt non erat. Maecenas dui ante, eleifend gravida sagittis quis, congue vestibulum enim.",
                     ],
+                    visi_misi: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis urna, vehicula ut felis non, venenatis lacinia urna."],
+                    title_artikel: "Lembaga Pemerintahan Desa Bahomoleo",
+                    subtitle_artikel: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales consequat dictum.",
                 },
             };
             this.imageUrl = apiResponse.data.imageUrl;
             this.paragraphs = apiResponse.data.paragraphs;
+            this.visi_misi = apiResponse.data.visi_misi;
+            this.title_artikel = apiResponse.data.title_artikel;
+            this.subtitle_artikel = apiResponse.data.subtitle_artikel;
         },
     },
     mounted() {
@@ -43,20 +103,72 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .bg-utama {
     background-image: linear-gradient(#003366, white);
-
 }
 
 .text-title {
     color: #fff;
+    font-weight: bold;
     text-align: center;
 }
 
 .custom-ukuran {
-    position:relative;
-    display: flex;
     width: 380px;
+    max-width: 100%;
+    display: block;
+    height: auto;
+}
+
+.penjelasan-title {
+    font-weight: 500;
+    color: black;
+    font-family: 'Poppins', sans-serif;
+}
+
+.text-visi {
+    color: #003366;
+    font-weight: bold;
+    text-align: center;
+}
+
+.penjelasan-vismis {
+    text-align: center;
+}
+
+.text-subtitle {
+    color: #003366;
+    font-weight: bold;
+    text-align: center;
+}
+
+.bg-kedua {
+    background-color: #D1D9E0;
+}
+
+.penjelasan-subtitle {
+    text-align: center;
+}
+
+.judul-artikel {
+    font-weight: 600;
+    color: #003366 !important;
+}
+
+.subjudul-artikel {
+    font-weight: bold;
+    font-size: 100%;
+}
+
+.btn-secondary {
+    background-color: #003366;
+    border: none;
+    padding: 10px 30px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 10px;
 }
 </style>
