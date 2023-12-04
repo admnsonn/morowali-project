52548103b23174e3ae9d0919899ed9a5ea4766f5
Aditya Fauzian Rifkikah Pati <55432346+Aditya-Fauzian-Rifkikah-Patty@users.noreply.github.com>
2023-12-04 21:13:20 +0700

    vite config build update
    vite config build update

--- Frontend/vue-project/vite.config.js
+++ Frontend/vue-project/vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({
   plugins: [
     vue(),
   ],
@@ -12,5 +12,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 })
+
+
