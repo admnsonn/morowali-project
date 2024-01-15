d9c22a391dfe0eabafe81c95e961ac06cfbb9de8
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-01-15 22:51:39 +0700

    update dikit

--- Frontend/vue-project/src/main.js
+++ Frontend/vue-project/src/main.js
@@ -1,7 +1,6 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
-import Navbar from './components/Navbar.vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
