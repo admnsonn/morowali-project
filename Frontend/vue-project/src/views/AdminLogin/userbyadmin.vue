4545a751c5a9a7001c781fb736226ac8309d24c5
Irfan Jesia Abyakta <irfanjesia31@gmail.com>
2024-01-16 14:22:35 +0700

    update table data warga

--- Frontend/vue-project/src/views/AdminLogin/userbyadmin.vue
+++ Frontend/vue-project/src/views/AdminLogin/userbyadmin.vue
@@ -1,12 +1,157 @@
 <template>
-    <div class="kontainer-admin">
-      <h1>Beranda Data Warga</h1>
-    </div>
-  </template>
+  <div class="kontainer-admin">
+    <h1>Beranda Data Warga</h1>
 
+    <!-- Tabel -->
+    <div class="table-container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NIK</th>
+            <th>Nama</th>
+            <th>KK</th>
+            <th>Jenis Kelamin</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in displayedData" :key="index">
+            <td>{{ item.id_pengguna }}</td>
+            <td>{{ item.nik }}</td>
+            <td>{{ item.nama_lengkap }}</td>
+            <td>{{ item.kk }}</td>
+            <td>{{ item.jenis_kelamin }}</td>
+            <td>CRUD</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <!-- Pagination -->
+      <div class="pagination">
+        <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        <span> {{ currentPage }} dari {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      tableData: [],
+      currentPage: 1,
+      itemsPerPage: 7, // Sesuaikan item table perhalaman
+    };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.tableData.length / this.itemsPerPage);
+    },
+    displayedData() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.tableData.slice(startIndex, endIndex);
+    },
+  },
+  methods: {
+    fetchData() {
+      const payload = { id_desa: parseInt(localStorage.getItem('desa_id')) };
+      
+      axios.post('http://localhost:8080/warga/list', payload)
+        .then(({ data }) => {
+          this.tableData = data.data;
+        })
+        .catch(error => {
+          console.error('Error in Axios POST request:', error);
+        });
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
 
 <style scoped>
-h1{
-    font-weight: 700;
+h1 {
+  font-weight: 700;
+}
+
+.table {
+  width: calc(100% - 70px);
+  border-collapse: collapse;
+  margin-top: 108px;
+}
+
+.table th,
+.table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.table th {
+  background-color: #003366;
+  color: white;
+}
+
+.table tbody tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+.pagination {
+  margin-right: 80px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: flex-end;
+  /* Memindahkan pagination ke sebelah kanan */
+  align-items: center;
+}
+
+.pagination button {
+  margin: 0 10px;
+  padding: 8px;
+  cursor: pointer;
+  border: 1px solid #ddd;
+  background-color: #003366;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  margin: 0 5px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.pagination button:disabled {
+  cursor: not-allowed;
+  color: #aaa;
+  border: 1px solid #ccc;
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.pagination span {
+  margin: 0 10px;
+  font-weight: bold;
+  color: #003366;
+}
+
+.table-container {
+  overflow-x: auto;
 }
 </style>
