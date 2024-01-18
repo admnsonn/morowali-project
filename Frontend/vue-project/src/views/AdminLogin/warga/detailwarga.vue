e311b6b7dc2f4365bf9b5d06ff6fd74d248485ad
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-01-18 21:19:37 +0700

    tambah data

--- Frontend/vue-project/src/views/AdminLogin/warga/detailwarga.vue
+++ Frontend/vue-project/src/views/AdminLogin/warga/detailwarga.vue
@@ -25,44 +25,102 @@
         <div class="field1">
           <div class="form-group">
             <label for="NamaLengkap">Nama Lengkap</label>
-            <input type="email" class="form-control" id="NamaLengkap" aria-describedby="nama"
-              placeholder="Enter email">
+            <input type="text" class="form-control" id="NamaLengkap" aria-label="nama" placeholder="masukan nama">
           </div>
+        </div>
 
-        </div>
-        <div class="field1">
+        <div class="field2">
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          
-        </div>
-        <div class="field1">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="JenisKelamin">Jenis Kelamin</label>
+            <input type="text" class="form-control" id="JenisKelamin" aria-label="kelamin"
+              placeholder="masukan jenis kelamin">
           </div>
         </div>
-        <div class="field1">
+
+        <div class="field3">
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="NomorTelpon">Nomer Telepon</label>
+            <input type="text" class="form-control" id="NomorTelpon" aria-label="hp" placeholder="masukan no hp">
           </div>
         </div>
-        <div class="field1">
+
+        <div class="field4">
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="NIK">Nomor Induk Kependudukan (NIK)</label>
+            <input type="text" class="form-control" id="NIK" aria-label="nik" placeholder="masukan nik">
           </div>
         </div>
+
+        <div class="field6">
+          <div class="form-group">
+            <label for="Alamat">Alamat</label>
+            <input type="text" class="form-control" id="Alamat" aria-label="alamat" placeholder="masukan alamat">
+          </div>
+        </div>
+
+        <div class="field7">
+          <div class="form-group">
+            <label for="Pendidikan">Pendidikan Terahir</label>
+            <input type="text" class="form-control" id="Pendidikan" aria-label="pendidikan"
+              placeholder="masukan pendidikan">
+          </div>
+        </div>
+
+        <div class="field8">
+          <div class="form-group">
+            <label for="KK">Nomor Kartu Keluarga (KK)</label>
+            <input type="text" class="form-control" id="KK" aria-label="kk" placeholder="masukan kartu keluarga">
+          </div>
+        </div>
+
+        <div class="field9">
+          <div class="form-group">
+            <label for="Agama">Agama</label>
+            <input type="text" class="form-control" id="Agama" aria-label="agama" placeholder="masukan agama">
+          </div>
+        </div>
+
+        <div class="field10">
+          <div class="form-group">
+            <label for="formFile" class="form-label">Foto Warga</label>
+            <input class="form-control" type="file" id="formFile">
+          </div>
+        </div>
+
+        <div class="field11">
+          <div class="form-group">
+            <label for="TTL">Tempat Lahir</label>
+            <input type="text" class="form-control" id="TTL" aria-label="ttl" placeholder="masukan tempat lahir">
+          </div>
+        </div>
+
+        <div class="field12">
+          <div class="form-group">
+            <label for="Status">Status Perkawinan</label>
+            <input type="text" class="form-control" id="Status" aria-label="ttl" placeholder="masukan status">
+          </div>
+        </div>
+
+        <div class="field13">
+          <button type="button" class="btn btn-success btn-simpan p-2 my-2" @click="tambahData">
+            <div class="nav-link router-link-underline teks-tambah">+ Tambah Data</div>
+          </button>
+        </div>
+
+        <div class="field14">
+          <div class="form-group">
+            <label for="Tanggal">Tanggal Lahir</label>
+            <input type="text" class="form-control" id="Tanggal" aria-label="tanggal" placeholder="masukan tanggal lahir">
+          </div>
+        </div>
+
+        <div class="field15">
+          <div class="form-group">
+            <label for="Pekerjaan">Pekerjaan</label>
+            <input type="text" class="form-control" id="Pekerjaan" aria-label="pekerjaan" placeholder="masukan pekerjaan">
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
@@ -74,42 +132,33 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      tableData: [],
-      currentPage: 1,
-      itemsPerPage: 7, // Sesuaikan item table perhalaman
+      formData: {
+        nama_lengkap: '',
+        jenis_kelamin: '',
+        no_telp: '',
+        nik: '',
+        alamat_pengguna: '',
+        profesi: '',
+        kk: '',
+        agama: '',
+        tanggal_lahir: '',
+        foto_pengguna: '',
+        TTL: '',
+        Status: '',
+      },
     };
-  },
-  computed: {
-    totalPages() {
-      return Math.ceil(this.tableData.length / this.itemsPerPage);
-    },
-    displayedData() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      const endIndex = startIndex + this.itemsPerPage;
-      return this.tableData.slice(startIndex, endIndex);
-    },
   },
   methods: {
     fetchData() {
       const payload = { id_desa: (localStorage.getItem('desa_id')) };
 
-      axios.post('http://localhost:8080/warga/list', payload)
+      axios.post('http://localhost:8080/warga/tambah', payload)
         .then(({ data }) => {
           this.tableData = data.data;
         })
         .catch(error => {
           console.error('Error in Axios POST request:', error);
         });
-    },
-    prevPage() {
-      if (this.currentPage > 1) {
-        this.currentPage--;
-      }
-    },
-    nextPage() {
-      if (this.currentPage < this.totalPages) {
-        this.currentPage++;
-      }
     },
   },
   mounted() {
@@ -166,5 +215,26 @@ h3 {
   grid-template-columns: auto auto auto;
   grid-gap: 40px;
   padding: 10px;
+  padding-right: 30px;
+}
+
+.btn-simpan {
+  background-color: #003366;
+  color: #fff;
+  border: none;
+}
+
+.btn-simpan:hover {
+  background-color: #003366;
+  color: #fff;
+  border: none;
+}
+
+.teks-tambah {
+  font-size: 15px;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  padding-left: 5px;
+  padding-right: 5px;
 }
 </style>
