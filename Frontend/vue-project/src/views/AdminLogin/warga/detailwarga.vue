fd1f9a52e8c5cf7a722e3d6645ac0d182aa67f47
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-01-19 00:24:45 +0700

    update

--- Frontend/vue-project/src/views/AdminLogin/warga/detailwarga.vue
+++ Frontend/vue-project/src/views/AdminLogin/warga/detailwarga.vue
@@ -25,43 +25,47 @@
         <div class="field1">
           <div class="form-group">
             <label for="NamaLengkap">Nama Lengkap</label>
-            <input v-model="nambahData.nama_lengkap" type="text" class="form-control" id="NamaLengkap" aria-label="nama" placeholder="masukan nama">
+            <input type="text" v-model="model.warga.nama_lengkap" class="form-control" id="NamaLengkap" aria-label="nama"
+              placeholder="masukan nama">
           </div>
         </div>
 
         <div class="field2">
           <div class="form-group">
             <label for="JenisKelamin">Jenis Kelamin</label>
-            <input v-model="nambahData.jenis_kelamin" type="text" class="form-control" id="JenisKelamin" aria-label="kelamin"
-              placeholder="masukan jenis kelamin">
+            <input type="text" v-model="model.warga.jenis_kelamin" class="form-control" id="JenisKelamin"
+              aria-label="kelamin" placeholder="masukan jenis kelamin">
           </div>
         </div>
 
         <div class="field3">
           <div class="form-group">
             <label for="NomorTelpon">Nomer Telepon</label>
-            <input v-model="nambahData.no_telp" type="text" class="form-control" id="NomorTelpon" aria-label="hp" placeholder="masukan no hp">
+            <input type="text" v-model="model.warga.no_telp" class="form-control" id="NomorTelpon" aria-label="hp"
+              placeholder="masukan no hp">
           </div>
         </div>
 
         <div class="field4">
           <div class="form-group">
             <label for="NIK">Nomor Induk Kependudukan (NIK)</label>
-            <input v-model="nambahData.nik" type="text" class="form-control" id="NIK" aria-label="nik" placeholder="masukan nik">
+            <input type="text" v-model="model.warga.nik" class="form-control" id="NIK" aria-label="nik"
+              placeholder="masukan nik">
           </div>
         </div>
 
         <div class="field6">
           <div class="form-group">
             <label for="Alamat">Alamat</label>
-            <input v-model="nambahData.alamat_pengguna" type="text" class="form-control" id="Alamat" aria-label="alamat" placeholder="masukan alamat">
+            <input type="text" v-model="model.warga.alamat_pengguna" class="form-control" id="Alamat" aria-label="alamat"
+              placeholder="masukan alamat">
           </div>
         </div>
 
         <div class="field7">
           <div class="form-group">
             <label for="Pendidikan">Pendidikan Terahir</label>
-            <input v-model="nambahData.profesi" type="text" class="form-control" id="Pendidikan" aria-label="pendidikan"
+            <input type="text" v-model="model.warga.profesi" class="form-control" id="Pendidikan" aria-label="pendidikan"
               placeholder="masukan pendidikan">
           </div>
         </div>
@@ -69,35 +73,47 @@
         <div class="field8">
           <div class="form-group">
             <label for="KK">Nomor Kartu Keluarga (KK)</label>
-            <input v-model="nambahData.kk" type="text" class="form-control" id="KK" aria-label="kk" placeholder="masukan kartu keluarga">
+            <input type="text" v-model="model.warga.kk" class="form-control" id="KK" aria-label="kk"
+              placeholder="masukan kartu keluarga">
           </div>
         </div>
 
         <div class="field9">
           <div class="form-group">
             <label for="Agama">Agama</label>
-            <input v-model="agama" type="text" class="form-control" id="Agama" aria-label="agama" placeholder="masukan agama">
+            <input type="text" v-model="model.warga.agama" class="form-control" id="Agama" aria-label="agama"
+              placeholder="masukan agama">
           </div>
         </div>
 
         <div class="field10">
           <div class="form-group">
             <label for="formFile" class="form-label">Foto Warga</label>
-            <input v-on="nambahData.foto_pengguna" class="form-control" type="file" id="formFile">
+            <input class="form-control" v-on:change="onFileChange" type="file" id="formFile">
           </div>
         </div>
 
         <div class="field11">
           <div class="form-group">
             <label for="TTL">Tempat Lahir</label>
-            <input v-model="nambahData.tempat_lahir" type="text" class="form-control" id="TTL" aria-label="ttl" placeholder="masukan tempat lahir">
+            <input type="text" v-model="model.warga.tempat_lahir" class="form-control" id="TTL" aria-label="ttl"
+              placeholder="masukan tempat lahir">
           </div>
         </div>
 
         <div class="field12">
           <div class="form-group">
             <label for="Status">Status Perkawinan</label>
-            <input v-model="nambahData.status_pernikahan" type="text" class="form-control" id="Status" aria-label="ttl" placeholder="masukan status">
+            <input type="text" v-model="model.warga.status_pernikahan" class="form-control" id="Status" aria-label="ttl"
+              placeholder="masukan status">
+          </div>
+        </div>
+
+        <div class="field14">
+          <div class="form-group">
+            <label for="Tanggal">Tanggal Lahir</label>
+            <input type="text" v-model="model.warga.tanggal_lahir" class="form-control" id="Tanggal" aria-label="tanggal"
+              placeholder="masukan tanggal lahir">
           </div>
         </div>
 
@@ -105,13 +121,6 @@
           <button type="button" class="btn btn-success btn-simpan p-2 my-2" @click="addNewData">
             <div class="nav-link router-link-underline teks-tambah">+ Tambah Data</div>
           </button>
-        </div>
-
-        <div class="field14">
-          <div class="form-group">
-            <label for="Tanggal">Tanggal Lahir</label>
-            <input v-model="nambahData.tanggal_lahir" type="text" class="form-control" id="Tanggal" aria-label="tanggal" placeholder="masukan tanggal lahir">
-          </div>
         </div>
       </div>
     </div>
@@ -122,78 +131,60 @@
 import axios from 'axios'
 
 export default {
+  name: 'wargaCreate',
   data() {
     return {
-      nambahData: {
-        nama_lengkap: '',
-        jenis_kelamin: '',
-        no_telp: '',
-        nik: '',
-        alamat_pengguna: '',
-        profesi: '',
-        kk: '',
-        agama: '',
-        foto_pengguna: '',
-        tanggal_lahir: '',
-        tempat_lahir: '',
-        status_pernikahan: '',
+      model: {
+        warga: {
+          nama_lengkap: '',
+          jenis_kelamin: '',
+          no_telp: '',
+          nik: '',
+          alamat_pengguna: '',
+          profesi: '',
+          kk: '',
+          agama: '',
+          foto_pengguna: '',
+          tanggal_lahir: '',
+          tempat_lahir: '',
+          status_pernikahan: '',
+        },
       },
+      tableData: [],
     };
   },
   methods: {
     addNewData() {
-      // Create the payload for the POST request
-      const payload = {
-        id_desa: localStorage.getItem("desa_id"),
+      axios.post("http://localhost:8080/warga/tambah", this.model.warga)
+        .then(res => {
+          console.log(res.data)
+          alert(res.data.message);
 
-        no_telp: this.nambahData.no_telp,
-        alamat_pengguna: this.nambahData.alamat_pengguna,
-        profesi: this.nambahData.profesi,
-        nik: this.nambahData.nik,
-        agama: this.nambahData.kk,
-        nama_lengkap: this.nambahData.nama_lengkap,
-        kk: this.nambahData.kk,
-        foto_pengguna: this.nambahData.foto_pengguna,
-        tanggal_lahir: this.nambahData.tanggal_lahir,
-        status_pernikahan: this.nambahData.status_pernikahan,
-        tempat_lahir: this.nambahData.tempat_lahir,
-        jenis_kelamin: this.nambahData.jenis_kelamin,
-      };
-
-      // Make a POST request to add new data
-      axios
-        .post("http://localhost:8080/warga/list", payload)
-        .then((response) => {
-          // Handle success, for example, refresh the data or show a success message
-          console.log("Data added successfully:", response.data);
-          // Refresh the data by calling fetchData or any other appropriate method
-          this.fetchData();
-          this.$router.push('/warga-desa')
-          // Clear the form fields
-          this.clearForm();
+          this.model.warga = {
+            nama_lengkap: '',
+            jenis_kelamin: '',
+            no_telp: '',
+            nik: '',
+            alamat_pengguna: '',
+            profesi: '',
+            kk: '',
+            agama: '',
+            foto_pengguna: '',
+            tanggal_lahir: '',
+            tempat_lahir: '',
+            status_pernikahan: '',
+          };
         })
-        .catch((error) => {
-          // Handle errors, for example, show an error message
-          console.error("Error in Axios POST request:", error);
+        .catch(error => {
+          console.error(error);
+          // Handle error, e.g., show an error message
         });
     },
-
-    clearForm() {
-      // Reset the form fields
-      this.newData = {
-        nama_lengkap: "",
-        jenis_kelamin: "",
-        no_telp: "",
-        nik: "",
-        alamat_pengguna: "",
-        profesi: "",
-        kk: "",
-        agama: "",
-        foto_pengguna: "",
-        tanggal_lahir: "",
-        tempat_lahir: "",
-        status_pernikahan: "",
-      };
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
     },
   },
 };
