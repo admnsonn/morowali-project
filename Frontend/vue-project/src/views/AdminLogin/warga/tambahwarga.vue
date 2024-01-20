ea7347d3def457dcbd52561317aad612016bbef8
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-01-20 15:46:56 +0700

    tambah data warga: clear

--- Frontend/vue-project/src/views/AdminLogin/warga/tambahwarga.vue
+++ Frontend/vue-project/src/views/AdminLogin/warga/tambahwarga.vue
@@ -22,161 +22,57 @@
     </div>
     <div class="isi-tambahdata">
       <div class="grid-container">
-        <div class="field1">
-          <div class="form-group">
-            <label for="NamaLengkap">Nama Lengkap</label>
-            <input
-              type="text"
-              v-model="model.warga.nama_lengkap"
-              class="form-control"
-              id="NamaLengkap"
-              aria-label="nama"
-              placeholder="masukan nama"
-            />
-          </div>
-        </div>
-
-        <div class="field2">
-          <div class="form-group">
-            <label for="JenisKelamin">Jenis Kelamin</label>
-            <input
-              type="text"
-              v-model="model.warga.jenis_kelamin"
-              class="form-control"
-              id="JenisKelamin"
-              aria-label="kelamin"
-              placeholder="masukan jenis kelamin"
-            />
-          </div>
-        </div>
-
-        <div class="field3">
-          <div class="form-group">
-            <label for="NomorTelpon">Nomer Telepon</label>
-            <input
-              type="text"
-              v-model="model.warga.no_telp"
-              class="form-control"
-              id="NomorTelpon"
-              aria-label="hp"
-              placeholder="masukan no hp"
-            />
-          </div>
-        </div>
-
         <div class="field4">
           <div class="form-group">
             <label for="NIK">Nomor Induk Kependudukan (NIK)</label>
-            <input
-              type="text"
-              v-model="model.warga.nik"
-              class="form-control"
-              id="NIK"
-              aria-label="nik"
-              placeholder="masukan nik"
-            />
+            <input type="text" v-model="model.warga.nik" class="form-control" id="NIK" aria-label="nik"
+              placeholder="masukan nik" />
           </div>
         </div>
 
-        <div class="field7">
+        <div class="field1">
           <div class="form-group">
-            <label for="Pendidikan">Pendidikan Terakhir</label>
-            <input type="text" v-model="model.warga.profesi" class="form-control" id="Pendidikan" aria-label="pendidikan"
-              placeholder="masukan pendidikan">
-          </div>
-        </div>
-
-        <div class="field6">
-          <div class="form-group">
-            <label for="Alamat">Alamat</label>
-            <textarea type="text" v-model="model.warga.alamat_pengguna" class="form-control" id="Alamat"
-              aria-label="alamat" placeholder="masukan alamat" rows="3"></textarea>
+            <label for="NamaLengkap">Nama Lengkap</label>
+            <input type="text" v-model="model.warga.nama_lengkap" class="form-control" id="NamaLengkap" aria-label="nama"
+              placeholder="masukan nama" />
           </div>
         </div>
 
         <div class="field8">
           <div class="form-group">
             <label for="KK">Nomor Kartu Keluarga (KK)</label>
-            <input
-              type="text"
-              v-model="model.warga.kk"
-              class="form-control"
-              id="KK"
-              aria-label="kk"
-              placeholder="masukan kartu keluarga"
-            />
+            <input type="text" v-model="model.warga.kk" class="form-control" id="KK" aria-label="kk"
+              placeholder="masukan kartu keluarga" />
           </div>
         </div>
 
-        <div class="field9">
+        <div class="field10">
           <div class="form-group">
-            <label for="Agama">Agama</label>
-            <input
-              type="text"
-              v-model="model.warga.agama"
+            <label for="formFile" class="form-label">Jenis Kelamin</label>
+            <br />
+            <select
+              ref="kategoriSelect"
+              v-model="model.warga.jenis_kelamin"
               class="form-control"
-              id="Agama"
-              aria-label="agama"
-              placeholder="masukan agama"
-            />
+              id="kategori_id"
+              aria-label="category"
+            >
+              <option value="" disabled selected>--Pilih Jenis Kelamin--</option>
+              <option value="1">Laki-Laki</option>
+              <option value="2">Perempuan</option>
+            </select>
           </div>
         </div>
 
         <div class="field10">
           <div class="form-group">
             <label for="formFile" class="form-label">Foto Warga</label>
-            <input class="form-control" @change="handleFileUpload($event)" type="file" id="formFile">
-          </div>
-        </div>
-
-        <div class="field11">
-          <div class="form-group">
-            <label for="TTL">Tempat Lahir</label>
-            <input
-              type="text"
-              v-model="model.warga.tempat_lahir"
-              class="form-control"
-              id="TTL"
-              aria-label="ttl"
-              placeholder="masukan tempat lahir"
-            />
-          </div>
-        </div>
-
-        <div class="field12">
-          <div class="form-group">
-            <label for="Status">Status Perkawinan</label>
-            <input
-              type="text"
-              v-model="model.warga.status_pernikahan"
-              class="form-control"
-              id="Status"
-              aria-label="ttl"
-              placeholder="masukan status"
-            />
-          </div>
-        </div>
-
-        <div class="field14">
-          <div class="form-group">
-            <label for="Tanggal">Tanggal Lahir</label>
-            <input
-              type="text"
-              v-model="model.warga.tanggal_lahir"
-              class="form-control"
-              id="Tanggal"
-              aria-label="tanggal"
-              placeholder="masukan tanggal lahir"
-            />
+            <input class="form-control" v-on:change="onFileChange" type="file" id="formFile">
           </div>
         </div>
 
         <div class="field13">
-          <button
-            type="button"
-            class="btn btn-success btn-simpan p-2 my-2"
-            @click="addNewData"
-          >
+          <button type="button" class="btn btn-success btn-simpan p-2 my-2" @click="addNewData">
             <div class="nav-link router-link-underline teks-tambah">
               + Tambah Data
             </div>
@@ -196,18 +92,11 @@ export default {
     return {
       model: {
         warga: {
-          nama_lengkap: "",
-          jenis_kelamin: "",
-          no_telp: "",
           nik: "",
-          alamat_pengguna: "",
-          profesi: "",
+          nama_lengkap: "",
           kk: "",
-          agama: "",
+          jenis_kelamin: "",
           foto_pengguna: "",
-          tanggal_lahir: "",
-          tempat_lahir: "",
-          status_pernikahan: "",
         },
       },
     };
@@ -219,38 +108,30 @@ export default {
         .then(res => {
           console.log(res.data)
           alert(res.data.message);
-          document.getElementById('formFile').value = '';
 
           this.model.warga = {
-            nama_lengkap: "",
-            jenis_kelamin: "",
-            no_telp: "",
             nik: "",
-            alamat_pengguna: "",
-            profesi: "",
+            nama_lengkap: "",
             kk: "",
-            agama: "",
+            jenis_kelamin: "",
             foto_pengguna: "",
-            tanggal_lahir: "",
-            tempat_lahir: "",
-            status_pernikahan: "",
           };
         })
         .catch((error) => {
           console.error("Error in Axios POST request:", error);
         });
     },
-    handleFileUpload(event) {
-      console.log('Event:', event);
+    onFileChange(e) {
+      // console.log('Event:', e);
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
 
-      // Check if event.target and event.target.files exist
-      if (event && event.target && event.target.files) {
-        this.model.warga.foto_pengguna = event.target.files[0];
-        console.log('Selected file:', this.model.warga.foto_pengguna);
-      } else {
-        console.error('Invalid event or file selection.');
-      }
+      this.model.warga.foto_pengguna = files[0].name;
     },
+  },
+  mounted() {
+    console.log(this.$refs.kategoriSelect.value);
+    this.$refs.kategoriSelect.focus();
   },
 };
 </script>
