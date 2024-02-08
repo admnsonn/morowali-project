74d08f711ab9456eecd90b7bdf6e258fe428ba95
Aditya Fauzian Rifkikah Pati <>
2024-02-08 22:01:21 +0700

    Pemerintahan NON LOGIN update consume API
    Pemerintahan NON LOGIN update consume API

--- Frontend/vue-project/src/views/pemerintah.vue
+++ Frontend/vue-project/src/views/pemerintah.vue
@@ -4,10 +4,16 @@
         <div class="container-lg mt-5">
             <div class="row">
                 <div class="col-md-6">
-                    <img :src="imageUrl" alt="Desa Bahomoleo" class="rounded mx-auto d-block custom-ukuran" />
+                    <img
+                    class="rounded mx-auto d-block custom-ukuran"
+                    :src="`data:image/png;base64,${this.imageUrl}`"
+                    alt="Desa"
+                    height="75"
+                    width="100"
+                  />
                 </div>
                 <div class="col-md-6">
-                    <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-title">{{ paragraph }}</p>
+                    <p class="penjelasan-title">{{ this.sambutan }}</p>
                 </div>
             </div>
         </div>
@@ -15,97 +21,123 @@
 
     <div class="container-lg mt-5">
         <h4 class="text-visi mb-4">Visi</h4>
-        <p v-for="(visis, index) in visi_misi" :key="index" class="penjelasan-vismis">{{ visis }}</p>
+        <p class="penjelasan-vismis">{{ this.visi }}</p>
     </div>
     <br>
     <div>
         <h4 class="text-visi mb-4">Misi</h4>
-        <p v-for="(visis, index) in visi_misi" :key="index" class="penjelasan-vismis">{{ visis }}</p>
+        <p class="penjelasan-vismis">{{ this.misi }}</p>
     </div>
 
     <br>
     <div class="mt-5 p-5 bg-kedua">
         <h2 class="text-subtitle mb-4 mt-0 pb-10">Program Kerja</h2>
         <div class="container-lg mt-5">
-            <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-subtitle">{{ paragraph }}</p>
+            <p class="penjelasan-subtitle">{{ this.proker_desa }}</p>
         </div>
     </div>
 
     <div class="mt-5 p-5">
         <h2 class="text-subtitle mb-4 mt-0 pb-10">Peraturan Kerja</h2>
         <div class="container-lg mt-5">
-            <p v-for="(paragraph, index) in paragraphs" :key="index" class="penjelasan-subtitle">{{ paragraph }}</p>
+            <p class="penjelasan-subtitle">{{ this.peraturan }}</p>
         </div>
     </div>
 
-    <div class="container py-5">
-        <div class="border rounded row align-items-center pt-3 pb-3 mb-4 with-shadow" :class="{ 'with-shadow': isHovered }"
-            @mouseenter="addShadow" @mouseleave="removeShadow">
-            <div class="row mt-4 mb-4">
-                <div class="col-md-12 order-md-1 text-center">
-                    <h3 class="mb-3 d-md-none judul-artikel">
-                        {{ title_artikel }}
-                    </h3>
-                    <div class="d-none d-md-block text">
-                        <h3 class="mb-2 judul-artikel text-center">
-                            {{ title_artikel }}
-                        </h3>
-                    </div>
-                    <br>
-                    <h5 class="mb-4 subjudul-artikel">{{ subtitle_artikel }}</h5>
-                    <a href="/lembaga">
-                        <button class="btn btn-secondary" @click="showDetail(item)">
-                            Selengkapnya
-                        </button>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-            isHovered: false,
-            imageUrl: "",
             paragraphs: "",
             visi_misi: "",
-            title_artikel: "",
-            subtitle_artikel: "",
+
+            imageUrl: "",
+            id_desa: "", 
+            sambutan: "",
+            visi: "",
+            misi: "",
+            proker_desa: "",
+            peraturan: "",
         };
     },
     methods: {
-        addShadow() {
-            this.isHovered = true;
-        },
-        removeShadow() {
-            this.isHovered = false;
+        async kepaladesa() {
+            try {
+                const response = await axios.get(`http://localhost:8080/pemerintah/kepdes/${this.id_desa}`, {});
+                if (response.data.status) { 
+                    this.imageUrl = response.data.kepala_desa_saat_ini[0].foto_kepala_desa
+                } else {
+                    console.log("Data Kosong atau Terjadi Kesalahan")
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         },
 
-        fetchData() {
-            const apiResponse = {
-                data: {
-                    imageUrl: "src/assets/img//kepdes1.png",
-                    paragraphs: [
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis urna, vehicula ut felis non, venenatis lacinia urna. Nullam eget nibh ante. Phasellus sed mi id turpis luctus congue ut sed neque. Pellentesque quis pretium justo, convallis imperdiet felis. Proin porttitor tristique varius.",
-                        "Integer non convallis dui, ac cursus neque. Curabitur facilisis scelerisque lacus quis bibendum. Mauris placerat dolor sem, sit amet vestibulum magna venenatis eu. Aliquam consectetur metus sapien, in gravida enim elementum id. Nulla et purus magna. Vestibulum porta vitae ipsum nec facilisis. Phasellus efficitur luctus enim vitae condimentum. Curabitur commodo purus ac mi luctus, vitae lobortis ante semper. Duis quam orci, rhoncus vitae ullamcorper quis, tincidunt non erat. Maecenas dui ante, eleifend gravida sagittis quis, congue vestibulum enim.",
-                    ],
-                    visi_misi: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In felis urna, vehicula ut felis non, venenatis lacinia urna."],
-                    title_artikel: "Lembaga Pemerintahan Desa Bahomoleo",
-                    subtitle_artikel: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales consequat dictum.",
-                },
-            };
-            this.imageUrl = apiResponse.data.imageUrl;
-            this.paragraphs = apiResponse.data.paragraphs;
-            this.visi_misi = apiResponse.data.visi_misi;
-            this.title_artikel = apiResponse.data.title_artikel;
-            this.subtitle_artikel = apiResponse.data.subtitle_artikel;
+        async katasambutan() {
+            try {
+                const response = await axios.get(`http://localhost:8080/pemerintah/sambutan/${this.id_desa}`, {});
+                if (response.data.status) { 
+                    this.sambutan = response.data.data[0].sambutan
+                } else {
+                    console.log("Data Kosong atau Terjadi Kesalahan")
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         },
+
+        async visimisi() {
+            try {
+                const response = await axios.get(`http://localhost:8080/pemerintah/visi-misi/${this.id_desa}`, {});
+                if (response.data.status) { 
+                    this.visi = response.data.data[0].visi_desa
+                    this.misi = response.data.data[0].misi_desa
+                } else {
+                    console.log("Data Kosong atau Terjadi Kesalahan")
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        },
+
+        async proker() {
+            try {
+                const response = await axios.get(`http://localhost:8080/pemerintah/proker/${this.id_desa}`, {});
+                if (response.data.status) { 
+                    this.proker_desa = response.data.data[0].proker
+                } else {
+                    console.log("Data Kosong atau Terjadi Kesalahan")
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        },
+
+        async peraturanDesa() {
+            try {
+                const response = await axios.get(`http://localhost:8080/pemerintah/peraturan/${this.id_desa}`, {});
+                if (response.data.status) { 
+                    this.peraturan = response.data.data[0].peraturan
+                } else {
+                    console.log("Data Kosong atau Terjadi Kesalahan")
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
     },
-    mounted() {
-        this.fetchData();
+    created() {
+        this.id_desa = localStorage.getItem("id_desa");
+        this.kepaladesa();
+        this.katasambutan();
+        this.visimisi();
+        this.proker();
+        this.peraturanDesa();
     },
 };
 </script>
